31fc7772e0b9ecb62b6877f777afe56407b95718
stevenpurcell <steven.ray.purcell@gmail.com>
2018-12-31 11:48:46 -0600

    Updated and parameterized 'About Me'. Removed Goughnut legend. Resized elements of sidenave.

--- personalWebsite/src/app/app.component.ts
+++ personalWebsite/src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Hey you guys!';
+  aboutMe = `I am a full-stack data scientist specializing in Python, Angular, and SQL. I use data analytics 
+  and machine learning tools to make predictions and make big data insightful. I write data pipelines, APIs, 
+  and backend tools for manipulating and reporting on that data and Angular apps for delivering that 
+  insight to clients.`
 }
